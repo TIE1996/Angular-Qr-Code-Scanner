264dd138f38fc6f5b148d7c3504bd29b67ad3aff
Patrick Will <patrick@www-will.de>
2019-07-04 17:29:27 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgQrScannerModule } from 'angular2-qrscanner';
 import { AppComponent } from './app.component';
+import { QrCodeReaderComponent } from './qr-code-reader/qr-code-reader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QrCodeReaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgQrScannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/qr-code-reader/qr-code-reader.component.ts
@@ -0,0 +1,46 @@
+import { Component, ViewChild, ViewEncapsulation, OnInit, AfterViewInit } from '@angular/core';
+import { QrScannerComponent } from 'angular2-qrscanner';
+
+@Component({
+  selector: 'app-qr-code-reader',
+  templateUrl: './qr-code-reader.component.html',
+  styleUrls: ['./qr-code-reader.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class QrCodeReaderComponent implements AfterViewInit  {
+
+  constructor() { }
+  @ViewChild(QrScannerComponent,{static: true })
+  qrScannerComponent: QrScannerComponent ;
+ 
+    ngAfterViewInit() {
+        this.qrScannerComponent.getMediaDevices().then(devices => {
+            console.log(devices);
+            const videoDevices: MediaDeviceInfo[] = [];
+            for (const device of devices) {
+                if (device.kind.toString() === 'videoinput') {
+                    videoDevices.push(device);
+                }
+            }
+            if (videoDevices.length > 0){
+                let choosenDev;
+                for (const dev of videoDevices){
+                    if (dev.label.includes('front')){
+                        choosenDev = dev;
+                        break;
+                    }
+                }
+                if (choosenDev) {
+                    this.qrScannerComponent.chooseCamera.next(choosenDev);
+                } else {
+                    this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+                }
+            }
+        });
+ 
+        this.qrScannerComponent.capturedQr.subscribe(result => {
+            console.log(result);
+        });
+    }
+
+}
